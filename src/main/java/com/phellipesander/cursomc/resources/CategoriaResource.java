0e3de1390bcee15f41548fae387211d8af45fc8d
Phellipe <phellipe.sander1@gmail.com>
2019-10-01 09:35:16 -0300

    Testando primeira entidade - Categoria

--- src/main/java/com/phellipesander/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/phellipesander/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package com.phellipesander.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.phellipesander.cursomc.entity.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1l, "informatica");
+		Categoria cat2 = new Categoria(2l, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 	
 }
