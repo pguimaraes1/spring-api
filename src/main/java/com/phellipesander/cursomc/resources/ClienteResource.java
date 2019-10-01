83482441768ea659aa5f2e1de68917dcc3f40cbe
Phellipe <phellipe.sander1@gmail.com>
2019-10-01 14:20:45 -0300

    endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/phellipesander/cursomc/resources/ClienteResource.java
@@ -0,0 +1,30 @@
+package com.phellipesander.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.phellipesander.cursomc.entity.Cliente;
+import com.phellipesander.cursomc.repositories.ClienteRepository;
+import com.phellipesander.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Long id) {
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
---
+++ src/main/java/com/phellipesander/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.phellipesander.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.phellipesander.cursomc.entity.Cliente;
+import com.phellipesander.cursomc.repositories.ClienteRepository;
+import com.phellipesander.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Long id) {
+		Optional<Cliente> obj = repo.findById(id);
+ 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
