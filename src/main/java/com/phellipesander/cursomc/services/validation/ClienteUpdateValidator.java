cbf570cc1b4e8839afa0ec3bba0c2175d314b091
Phellipe <phellipe.sander1@gmail.com>
2019-10-04 12:58:04 -0300

    Validacao customizada - email nao repetido no update de Cliente

---
+++ src/main/java/com/phellipesander/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,56 @@
+package com.phellipesander.cursomc.services.validation;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.phellipesander.cursomc.dto.ClienteDTO;
+import com.phellipesander.cursomc.entity.Cliente;
+import com.phellipesander.cursomc.entity.enums.TipoCliente;
+import com.phellipesander.cursomc.repositories.ClienteRepository;
+import com.phellipesander.cursomc.resources.exception.FieldMessage;
+import com.phellipesander.cursomc.services.validation.utils.BR;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		
+		List<FieldMessage> list = new ArrayList<>();
+
+		// inclua os testes aqui, inserindo erros na lista
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldNome())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
