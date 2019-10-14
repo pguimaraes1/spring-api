159b99efc6364836eecd253730e4fbb333de09b3
Phellipe Sander <phellipe.sander1@gmail.com>
2019-10-13 21:17:32 -0300

    Email HTML - thymeleaf

--- src/main/java/com/phellipesander/cursomc/services/EmailService.java
+++ src/main/java/com/phellipesander/cursomc/services/EmailService.java
@@ -1,11 +1,18 @@
 package com.phellipesander.cursomc.services;
 
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.phellipesander.cursomc.entity.Pedido;
 
 public interface EmailService {
+	//texto plano
 	void sendOrderConfirmationEmail(Pedido obj);
 	void sendEmail(SimpleMailMessage msg);
+	
+	//html
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
 }
