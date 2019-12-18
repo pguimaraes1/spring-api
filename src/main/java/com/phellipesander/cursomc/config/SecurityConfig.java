0a23a47d1b4b292c6420148ce9bbacb1a76049f3
s4nderx <phellipe.sander1@gmail.com>
2019-12-18 01:32:54 -0300

    Implementando Autorizacao

--- src/main/java/com/phellipesander/cursomc/config/SecurityConfig.java
+++ src/main/java/com/phellipesander/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.phellipesander.cursomc.security.JWTAuthenticationFilter;
+import com.phellipesander.cursomc.security.JWTAuthorizationFilter;
 import com.phellipesander.cursomc.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
