d467598b0b5a8b34938abcdec56b8cc2f60ff186
s4nderx <phellipe.sander1@gmail.com>
2019-12-18 01:07:03 -0300

    Implementando autenticacao e geracao de token JWT

---
+++ src/main/java/com/phellipesander/cursomc/security/JWTAuthenticationFilter.java
@@ -0,0 +1,51 @@
+package com.phellipesander.cursomc.security;
+
+import java.util.ArrayList;
+
+import javax.naming.AuthenticationException;
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.phellipesander.cursomc.dto.CredenciaisDTO;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	private AuthenticationManager authenticationManager;
+
+	private JWTUtil jwtUtil;
+
+	public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+		this.authenticationManager = authenticationManager;
+		this.jwtUtil = jwtUtil;
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+		try {
+			CredenciaisDTO creds = new ObjectMapper().readValue(request.getInputStream(), CredenciaisDTO.class);
+			UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getSenha(), new ArrayList<>());
+			
+			Authentication auth = authenticationManager.authenticate(authToken);
+			return auth;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication auth) {
+		
+		String username = ((UserSS) auth.getPrincipal()).getUsername();
+		String token = jwtUtil.generateToken(username);
+		response.addHeader("Authorization", "Bearer " + token);
+
+	}
+
+}
