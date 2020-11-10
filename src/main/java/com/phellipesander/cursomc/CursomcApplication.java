82f0db819357b3f7c136f5422b027e97a43db606
Phellipe Sander <phellipe.sander1@gmail.com>
2020-11-10 16:10:56 -0300

    Salvando primeiro arquivo no s3

--- src/main/java/com/phellipesander/cursomc/CursomcApplication.java
+++ src/main/java/com/phellipesander/cursomc/CursomcApplication.java
@@ -1,5 +1,7 @@
 package com.phellipesander.cursomc;
 
+import com.phellipesander.cursomc.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,15 +9,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
-
-
+		s3Service.uploadFile("C:\\Users\\Sander\\Pictures\\eu.jpg");
 	}
 
 }
